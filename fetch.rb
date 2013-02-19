afd75cd3d4b083b5f6662de03d8c2db23beeaae4
luke-liu <liu.flydragon@gmail.com>
2013-02-19 22:30:31 +1100

    added post request. need cookie and xml parse support.

--- fetch.rb
+++ fetch.rb
@@ -18,4 +18,25 @@ def fetch(uri)
 	end
 end
 
-puts fetch("https://jigsaw.thoughtworks.com/");
+fetch("https://jigsaw.thoughtworks.com/");
+def post(uri)
+	uri = URI.parse(uri);
+	http = Net::HTTP.new(uri.host, uri.port);
+	http.use_ssl = true;
+
+	request = Net::HTTP::Post.new(uri.path);
+	request.set_form_data('username' => 'ljliu', 'password' => '42031341');
+	res = http.start{|http| http.request(request)}
+	case res
+		when Net::HTTPRedirection then
+			location = res['location']
+			warn "redirected to #{location}"
+			fetch(location)
+		else
+			res;
+	end
+end
+
+puts "submit"
+puts post("https://cas.thoughtworks.com/cas/login?service=https%3A%2F%2Fjigsaw.thoughtworks.com%2F");
+
